7d5363880a98610ef4e6f3b462b06fa624ca5175
Pierre <mfpierre@gmail.com>
2014-11-06 22:22:15 +0100

    Add the question list

---
+++ client/templates/questions/questions_list.js
@@ -0,0 +1,12 @@
+var questionsData = [
+  {
+    title: 'What is the meaning of life ?'
+  },
+  {
+    title: 'Does this site work ?'
+  }
+];
+
+Template.questionsList.helpers({
+  questions: questionsData
+});
