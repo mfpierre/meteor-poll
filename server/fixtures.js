040df72bce1529e700eb7e4ff0c7bbfcff9db154
Pierre <mfpierre@gmail.com>
2014-11-07 15:05:56 +0100

    Add answers to fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,9 +1,37 @@
 if (Questions.find().count() === 0) {
   Questions.insert({
-    title: 'What is the meaning of life ?'
+    title: 'What do you think about Meteor JS ?',
+    answers: [
+      {
+        title: 'It\'s awesome',
+        count: 0
+      },
+      {
+        title: 'Did\'nt try it yet, but seems great',
+        count: 0
+      },
+      {
+        title: 'Not convinced',
+        count: 0
+      },
+      {
+        title: 'I don\'t know',
+        count: 0
+      }
+    ]
   });
 
   Questions.insert({
-    title: 'Does this site work ?'
+    title: 'Does this site even work ?',
+    answers: [
+      {
+        title: 'Yes',
+        count: 0
+      },
+      {
+        title: 'No',
+        count: 0
+      }
+    ]
   });
 }
