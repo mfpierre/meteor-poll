28c428fbda09828bcced2b5d919bee5f1df9f92c
Pierre <mfpierre@gmail.com>
2014-11-12 15:01:17 +0100

    Update fixtures with separated answers

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,37 +1,45 @@
 if (Questions.find().count() === 0) {
-  Questions.insert({
+  var question1 = Questions.insert({
     title: 'What do you think about Meteor JS ?',
-    answers: [
-      {
-        title: 'It\'s awesome',
-        count: 0
-      },
-      {
-        title: 'Did\'nt try it yet, but seems great',
-        count: 0
-      },
-      {
-        title: 'Not convinced',
-        count: 0
-      },
-      {
-        title: 'I don\'t know',
-        count: 0
-      }
-    ]
   });
 
-  Questions.insert({
-    title: 'Does this site even work ?',
-    answers: [
-      {
-        title: 'Yes',
-        count: 0
-      },
-      {
-        title: 'No',
-        count: 0
-      }
-    ]
+  Answers.insert({
+    question_id: question1,
+    title: 'It\'s awesome',
+    count: 0
+  });
+
+  Answers.insert({
+    question_id: question1,
+    title: 'Did\'nt try it yet, but seems great',
+    count: 0
+  });
+
+  Answers.insert({
+    question_id: question1,
+    title: 'Not convinced',
+    count: 0
+  });
+
+  Answers.insert({
+    question_id: question1,
+    title: 'I don\'t know',
+    count: 0
+  });
+
+  var question2 = Questions.insert({
+    title: 'Does this site even work ?'
+  });
+
+  Answers.insert({
+    question_id: question2,
+    title: 'Yes',
+    count: 0
+  });
+
+  Answers.insert({
+    question_id: question2,
+    title: 'No',
+    count: 0
   });
 }
