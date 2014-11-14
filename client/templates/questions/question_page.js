9ffc78b583b4d128b16ec609404c966ad7cd2bc4
Pierre <mfpierre@gmail.com>
2014-11-14 14:39:22 +0100

    Add reactive chart

--- client/templates/questions/question_page.js
+++ client/templates/questions/question_page.js
@@ -1,5 +1,38 @@
+function drawChart(data) {
+  var answers = data || Answers.find({question_id: this._id}).fetch();
+
+  var countData = _.map(answers, function(answer){ return answer.count });
+
+  if(!$('.ct-chart').length) {
+    return;
+  }
+
+  if(!countData.length || _.reduce(countData, function(memo, num){ return memo + num; }, 0) === 0) {
+    return;
+  }
+
+  var data = {
+    series: countData
+  };
+
+  var pieChart = new Chartist.Pie('.ct-chart', data, {
+    labelInterpolationFnc: function(value) {
+      return Math.round(value / data.series.reduce(function(a, b) { return a + b }) * 100) + '%';
+    }
+  });
+}
+
 Template.questionPage.helpers({
   answers: function() {
     return Answers.find({question_id: this._id});
-  }
+  },
+  drawChart: drawChart
 });
+
+Template.questionPage.rendered = function() {
+  var data = $('.answer-item').map(function(){
+      return $(this).data();
+  }).get();
+
+  drawChart(data);
+}
